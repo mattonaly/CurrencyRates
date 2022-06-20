511ae4606a376e8e3e089863507e0ecb74b97d26
mattonaly <mattioskar.mat@gmail.com>
2022-06-20 13:22:05 +0200

    feat: add table with styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,128 @@
-import { Component } from '@angular/core';
+import { RatesService } from './services/rates.service';
+import { Rate } from './models/Rate';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Table } from 'primeng/table';
+import { takeUntil, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'CurrencyRates';
+export class AppComponent implements OnInit, OnDestroy {
+  destroy$ = new Subject();
+
+  rateItems$ = this.ratesServices.getRates$().pipe(
+    takeUntil(this.destroy$),
+    tap((rates) => {
+      this.rates = rates;
+      this.loading = false;
+    })
+  );
+
+  rates: Rate[] = [];
+
+  loading: boolean = true;
+
+  date: Date = new Date();
+
+  error = {
+    notFound: false,
+    badRequest: false,
+    limitExceeded: false,
+    invalidDate: false,
+  };
+
+  activityValues: number[] = [0, 100];
+
+  stateOptions = [
+    { label: 'Ciemny', value: 'dark' },
+    { label: 'Jasny', value: 'light' },
+  ];
+
+  darkMode: string = 'dark';
+
+  constructor(private readonly ratesServices: RatesService) {
+    this.rateItems$.subscribe();
+  }
+
+  ngOnInit(): void {
+    if (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+
+  clear(table: Table) {
+    table.clear();
+  }
+
+  onCalendarChange(event: any) {
+    if (
+      this.error.notFound ||
+      this.error.badRequest ||
+      this.error.limitExceeded ||
+      this.error.invalidDate
+    ) {
+      this.error.notFound = false;
+      this.error.badRequest = false;
+      this.error.limitExceeded = false;
+      this.error.invalidDate = false;
+    }
+
+    if (event > new Date()) {
+      this.error.invalidDate = true;
+      return;
+    }
+
+    this.loading = true;
+
+    this.ratesServices.getRatesOnDay$(this.date).subscribe(
+      (rates) => {
+        this.rates = rates;
+        this.loading = false;
+      },
+      (error) => {
+        this.rates = [];
+        this.loading = false;
+
+        if (error.status === 404) {
+          this.error.notFound = true;
+        }
+
+        if (
+          error.status === 400 &&
+          error.error.message === 'Bad Request - Limit exceeded'
+        ) {
+          this.error.limitExceeded = true;
+          return;
+        }
+
+        if (error.status === 400) {
+          this.error.badRequest = true;
+        }
+      }
+    );
+  }
+
+  onThemeChange(event: any) {
+    if (event.value === 'dark') {
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.removeItem('theme');
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SliderModule } from 'primeng/slider';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TableModule,
+    CalendarModule,
+    SliderModule,
+    DialogModule,
+    MultiSelectModule,
+    ContextMenuModule,
+    DropdownModule,
+    ButtonModule,
+    ToastModule,
+    InputTextModule,
+    ProgressBarModule,
+    SelectButtonModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: LOCALE_ID, useValue: 'pl-PL' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
